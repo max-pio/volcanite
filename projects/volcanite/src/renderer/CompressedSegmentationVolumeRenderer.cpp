4c5614a91e46d5fd6d74bde3faae9aa844b16b80
Max Piochowiak <max.piochowiak@kit.edu>
2024-04-05 15:55:52 +0200

    fix crash when false splitting plane limits where entered

--- projects/volcanite/src/renderer/CompressedSegmentationVolumeRenderer.cpp
+++ projects/volcanite/src/renderer/CompressedSegmentationVolumeRenderer.cpp
@@ -736,6 +736,8 @@ void CompressedSegmentationVolumeRenderer::initGui(vvv::GuiInterface *gui) {
                 ImGui::DragFloatRange2("Splitting Plane X", &m_bboxMin.x, &m_bboxMax.x, 0.01f, 0.0f, 1.f, "Min: %.2f %%", "Max: %.2f %%");
                 ImGui::DragFloatRange2("Splitting Plane Y", &m_bboxMin.y, &m_bboxMax.y, 0.01f, 0.0f, 1.f, "Min: %.2f %%", "Max: %.2f %%");
                 ImGui::DragFloatRange2("Splitting Plane Z", &m_bboxMin.z, &m_bboxMax.z, 0.01f, 0.0f, 1.f, "Min: %.2f %%", "Max: %.2f %%");
+                m_bboxMin = glm::clamp(m_bboxMin, glm::vec3(0.f), glm::vec3(1.f));
+                m_bboxMax = glm::clamp(m_bboxMax, m_bboxMin, glm::vec3(1.f));
             },
             "Splitting Planes");
 #endif
