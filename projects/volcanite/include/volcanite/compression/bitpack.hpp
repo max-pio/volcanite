ec9ca1cbbd294705b401a0386a696b51d2b97122
Max Piochowiak <max.piochowiak@kit.edu>
2024-07-21 12:49:09 +0200

    fix size_t namespace issue in bitpack

--- projects/volcanite/include/volcanite/compression/bitpack.hpp
+++ projects/volcanite/include/volcanite/compression/bitpack.hpp
@@ -25,24 +25,24 @@ namespace volcanite {
  * Returns the entry_id-th 4 bit entry from the memory block starting at v[start].
  * Note that start counts in elements of size uint32_t and entry_id counts in elements of 4 bit size.
  */
-uint32_t read4Bit(const uint32_t* v, size_t start, size_t entry_id);
+uint32_t read4Bit(const uint32_t* v, std::size_t start, std::size_t entry_id);
 
 /**
  * Returns the entry_id-th 4 bit entry from the memory block starting at v[start].
  * Note that start counts in elements of size uint32_t and entry_id counts in elements of 4 bit size.
  */
-uint32_t read4Bit(const std::vector<uint32_t> &v, size_t start, size_t entry_id);
+uint32_t read4Bit(const std::vector<uint32_t> &v, std::size_t start, std::size_t entry_id);
 
 
 /**
  * Writes the value value4bit to the entry_id-th position in v, offset by start uint32_t elements.
  */
-void write4Bit(uint32_t* v, size_t start, size_t entry_id, uint32_t value4bit);
+void write4Bit(uint32_t* v, std::size_t start, std::size_t entry_id, uint32_t value4bit);
 
 /**
  * Writes the value value4bit to the entry_id-th position in v, offset by start uint32_t elements.
  */
-void write4Bit(std::vector<uint32_t> &v, size_t start, size_t entry_id, uint32_t value4bit);
+void write4Bit(std::vector<uint32_t> &v, std::size_t start, std::size_t entry_id, uint32_t value4bit);
 
 
 /**
@@ -50,7 +50,7 @@ void write4Bit(std::vector<uint32_t> &v, size_t start, size_t entry_id, uint32_t
  * If the number of elements is not divided evenly, the last resulting element does not contain all entries, i.e. is padded.
  * @return the number of 4bit elements that were squeezed
  */
-uint32_t pack4Bit(std::vector<uint32_t> &v, size_t start, size_t end);
+uint32_t pack4Bit(std::vector<uint32_t> &v, std::size_t start, std::size_t end);
 
 
 std::vector<uint8_t> convertPacked32bit2PackedByte(const std::vector<uint32_t> &v);
--- projects/volcanite/src/volcanite/compression/bitpack.cpp
+++ projects/volcanite/src/volcanite/compression/bitpack.cpp
@@ -21,7 +21,7 @@ namespace volcanite {
  * Returns the entry_id-th 4 bit entry from the memory block starting at v[start].
  * Note that start counts in elements of size uint32_t and entry_id counts in elements of 4 bit size.
  */
-    uint32_t read4Bit(const uint32_t *v, size_t start, size_t entry_id) {
+    uint32_t read4Bit(const uint32_t *v, std::size_t start, std::size_t entry_id) {
         // we packed into the highest bits first: one uint contains the indices [0000111122223333 ... 7777]
         //assert(start + entry_id / 8 < v.size() && "trying to unpack element out of bounds");
         int shift = 28 - 4 * static_cast<int>(entry_id % 8);
@@ -33,7 +33,7 @@ namespace volcanite {
  * Returns the entry_id-th 4 bit entry from the memory block starting at v[start].
  * Note that start counts in elements of size uint32_t and entry_id counts in elements of 4 bit size.
  */
-    uint32_t read4Bit(const std::vector<uint32_t> &v, size_t start, size_t entry_id) {
+    uint32_t read4Bit(const std::vector<uint32_t> &v, std::size_t start, std::size_t entry_id) {
         // we packed into the highest bits first: one uint contains the indices [0000111122223333 ... 7777]
         assert(start + entry_id / 8 < v.size() && "trying to unpack element out of bounds");
         int shift = 28 - 4 * static_cast<int>(entry_id % 8);
@@ -45,7 +45,7 @@ namespace volcanite {
 /**
  * Writes the value value4bit to the entry_id-th position in v, offset by start uint32_t elements.
  */
-    void write4Bit(uint32_t *v, size_t start, size_t entry_id, uint32_t value4bit) {
+    void write4Bit(uint32_t *v, std::size_t start, std::size_t entry_id, uint32_t value4bit) {
         assert(value4bit < 16 && "value must be within 4 bit range");
 
         int shift = 28 - 4 * static_cast<int>(entry_id % 8);
@@ -58,7 +58,7 @@ namespace volcanite {
 /**
  * Writes the value value4bit to the entry_id-th position in v, offset by start uint32_t elements.
  */
-    void write4Bit(std::vector<uint32_t> &v, size_t start, size_t entry_id, uint32_t value4bit) {
+    void write4Bit(std::vector<uint32_t> &v, std::size_t start, std::size_t entry_id, uint32_t value4bit) {
         assert(value4bit < 16 && "value must be within 4 bit range");
         assert(start + entry_id / 8 < v.size() && "trying to write element out of bounds");
 
@@ -75,14 +75,14 @@ namespace volcanite {
  * If the number of elements is not divided evenly, the last resulting element does not contain all entries, i.e. is padded.
  * @return the number of 4bit elements that were squeezed
  */
-    uint32_t pack4Bit(std::vector<uint32_t> &v, size_t start, size_t end) {
+    uint32_t pack4Bit(std::vector<uint32_t> &v, std::size_t start, std::size_t end) {
         // iterate over all 32 bit elements, pack them into 4 bit elements in the same array
-        size_t i = start;
+        std::size_t i = start;
         for (; i < end; i++) {
             write4Bit(v.data(), start, i, v[i]);
         }
         // round up: how many elements did we write divided by packing factor
-        size_t new_size = (i - start + 7) / 8;
+        std::size_t new_size = (i - start + 7) / 8;
 
         // resize the vector (this won't change the capacity / reserve a new array)
         v.erase(v.begin() + static_cast<long>(start + new_size), v.begin() + static_cast<long>(end));
@@ -103,7 +103,7 @@ namespace volcanite {
 
     std::vector<uint8_t> unpackHalfByte2Byte(std::vector<uint8_t> &v) {
         std::vector<uint8_t> out(v.size() * 2);
-        for (size_t i = 0; i < v.size(); i++) {
+        for (std::size_t i = 0; i < v.size(); i++) {
             out.at(i * 2) = (v[i] >> 4);
             out.at(i * 2 + 1) = (v[i] & 0xF);
         }
