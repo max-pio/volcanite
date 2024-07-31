5dcd664e01b770653644aca1b2b611865948129c
Max Piochowiak <max.piochowiak@kit.edu>
2024-07-31 13:13:04 +0200

    BitVector without rank

--- projects/volcanite/include/volcanite/compression/BitVector.hpp
+++ projects/volcanite/include/volcanite/compression/BitVector.hpp
@@ -17,28 +17,125 @@
 
 #include <cstdint>
 #include <bit>
+#include <cassert>
+#include <vector>
+#include <string>
+#include <sstream>
 
 namespace volcanite {
 
+/// Words are the bit vector atomic storage unit and store bits in reverse order.
+typedef uint64_t BVWordType;
+/// Bits covered by one word.
+static constexpr uint32_t WORD_BIT_SIZE = sizeof(BVWordType) * 8u;
 
-typedef uint32_t BitWordType;
+// To stick to the naming conventions, some of the following constants are taken from pasta::bit_vector:
+// (C) 2021 Florian Kurpicz <florian@kurpicz.org>, released under the GPLv3 license
+// https://github.com/pasta-toolbox/bit_vector
 
-inline uint32_t rank1(BitWordType value, uint32_t index) {
-    return std::popcount(value << (32u - index));
+/// Bits covered by an L2-block.
+static constexpr uint32_t L2_BIT_SIZE = 512;
+/// Bits covered by an L1-block.
+static constexpr uint32_t L1_BIT_SIZE = 8 * L2_BIT_SIZE;
+
+/// Number of 64-bit words covered by an L2-block.
+static constexpr uint32_t L2_WORD_SIZE = L2_BIT_SIZE / (sizeof(uint64_t) * 8);
+/// Number of 64-bit words covered by an L1-block.
+static constexpr uint32_t L1_WORD_SIZE = L1_BIT_SIZE / (sizeof(uint64_t) * 8);
+
+inline uint32_t rank1(BVWordType value, uint32_t index) {
+    return std::popcount(value << (WORD_BIT_SIZE - index));
 }
 
-inline uint32_t access(BitWordType value, uint32_t index) {
-    return (value >> (32u - index)) & 1u;
-}
 
-inline void set
 
-/// A bitvector implementation for wavelet matrices that is close to a C-style implementation.
-/// It supports the rank0, rank1 and access operations on 32 bit unsigned integers.
-/// This allows to easily port the code to GLSL shader code. Open question: would using uvec4 as base elements, or
-/// uint64 as base elements have performance improvements?
+
+/// A bitvector implementation for wavelet matrices that is close to a C- or GLSL-style implementation.
+/// It supports the rank0, rank1 and access operations.
+/// Open question: could we use uvec4 as base elements?
 class BitVector {
 
+    // Some notes on optimizations:
+    // * within a word, the bits are stored in reverse order, i.e. the first bit is the LSB. This saves one subtraction
+    //   before computing the shift for the access operation.
+    // * in theory, >> 6 would be faster than dividing by a BIT_WORD_SIZE of 64. But the compiler optimizes this for us.
+    // * in theory, & 0b111111 would be faster than computing modulo by a BIT_WORD_SIZE of 64. But the compiler
+    //   optimizes this for us. Therefore, we use the / and % notation for better readability.
+
+public:
+    BitVector() : m_size(0u), m_data() {}
+    BitVector(uint32_t size) : m_size(size), m_data(words_for_size(size)) {}
+
+    BitVector(uint32_t size, uint8_t bit) : m_size(size),
+                                            m_data(words_for_size(size), bit ? ~0ull : 0ull) {}
+
+   [[nodiscard]] uint8_t access(uint32_t index) const {
+       return (m_data[index / WORD_BIT_SIZE] >> index) & 1u;
+    }
+
+    void set(uint32_t index, uint8_t bit_value) {
+        assert(index < m_size && "trying to set bit in bit vector that is out of bounds.");
+        assert(bit_value <= 1u && "bit_value must be 0 or 1.");
+
+        // https://graphics.stanford.edu/~seander/bithacks.html#ConditionalSetOrClearBitsWithoutBranching
+        // the compiler optimizes modulo and division into bit shift instructions (which require fewer cycles)
+        BVWordType mask = 1ull << (index % WORD_BIT_SIZE);
+        m_data[index / WORD_BIT_SIZE] = (m_data[index / WORD_BIT_SIZE] & ~mask) | (-static_cast<BVWordType>(bit_value) & mask);
+    }
+
+    /// Resizes the vector so that it stores size many bits.
+    void resize(uint32_t size) {
+        m_size = size;
+        m_data.resize(words_for_size(size));
+    }
+
+    /// Reserves space for size many bits in memory without altering the bit vectors actual size.
+    void reserve(uint32_t size) {
+        m_data.resize(words_for_size(m_size));
+    }
+
+    /// Removes all unused memory space if capacity() is greater than size().
+    /// Note that this is a non-binding request to the underlying BVWordType std::vector.
+    void shrink_to_fit() {
+        uint32_t target_size = words_for_size(m_size);
+        if (capacity() > target_size)
+            m_data.resize(target_size);
+        m_data.shrink_to_fit();
+    }
+
+    /// Appends bit_value to the end of the bit vector. If this requires a capacity increase, the bit vector's current
+    /// capacity is doubled.
+    /// @param bit_value the bit value to append to the vector. Must be either 0 or 1.
+    void push_back(uint8_t bit_value) {
+        m_size++;
+        if (m_size > capacity())
+            reserve(m_size * 2u);
+        set(m_size - 1, bit_value);
+    }
+
+    [[nodiscard]] uint32_t size() const { return m_size; }
+    [[nodiscard]] uint32_t capacity() const { return m_data.size() * WORD_BIT_SIZE; }
+
+    [[nodiscard]] std::string str() const {
+        std::stringstream ss;
+        for (uint32_t i = 0; i < m_size; i++) {
+            ss << (access(i) ? '1' : '0');
+            if (i % WORD_BIT_SIZE == WORD_BIT_SIZE - 1u && i < m_size - 1u)
+                ss << " ";
+        }
+        return ss.str();
+    }
+
+    [[nodiscard]] const BVWordType* rawData() const { return &m_data[0]; }
+    [[nodiscard]] uint32_t rawDataSize() const { return words_for_size(m_size); }
+
+private:
+
+    /// @return how many BVWordType entries are needed to store size many bits.
+    static inline uint32_t words_for_size(uint32_t size) { return (size + WORD_BIT_SIZE - 1u) / WORD_BIT_SIZE; }
+
+    uint32_t m_size;                    ///< number of bits stored in the bit vector
+    std::vector<BVWordType> m_data;     ///< the raw data array storing bits in BVWordType words
 
 };
 
---
+++ projects/volcanite/src/bin/bitvector_test.cpp
@@ -0,0 +1,99 @@
+//  Copyright (C) 2024, Max Piochowiak, Karlsruhe Institute of Technology
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "vvv/headless_entrypoint.hpp"
+#include "volcanite/compression/BitVector.hpp"
+
+#include <iostream>
+#include <string>
+#include <random>
+
+using namespace volcanite;
+
+std::random_device rd;
+std::mt19937 mt(rd());
+std::uniform_real_distribution<double> dist(0., 1.);
+
+std::string booleansStr(const std::vector<bool>& bv) {
+    std::stringstream ss;
+    for (uint32_t i = 0; i < bv.size(); i++) {
+        ss << (bv[i] ? '1' : '0');
+        if (i % WORD_BIT_SIZE == WORD_BIT_SIZE - 1u && i <  bv.size() - 1u)
+            ss << " ";
+    }
+    return ss.str();
+}
+
+int test_set_access(uint32_t size = 100) {
+    std::cout << "test: set access linear" << std::endl;
+    BitVector bitVector = BitVector(size);
+    std::vector<bool> boolVector(size);
+
+    for (uint32_t i = 0; i < size; i++) {
+        boolVector[i] = dist(mt) >= 0.5;
+        bitVector.set(i, boolVector[i]);
+    }
+    std::cout << booleansStr(boolVector) << std::endl;
+    std::cout << bitVector.str() << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "test: set access random" << std::endl;
+    for (uint32_t i = 0u; i < size / 2u; i++) {
+        // switch bits at random positions
+        uint32_t random_pos = static_cast<uint32_t>(dist(mt) * size);
+        boolVector[random_pos] = !boolVector[random_pos];
+
+        bitVector.set(random_pos, boolVector[random_pos]);
+    }
+    std::cout << booleansStr(boolVector) << std::endl;
+    std::cout << bitVector.str() << std::endl;
+
+    std::cout << std::endl;
+
+    return 0;
+}
+
+int test_rank() {
+    std::cout << "test: push_back rank1 on word level" << std::endl;
+
+    BitVector bitVector;
+
+    for (uint32_t i = 0; i < WORD_BIT_SIZE; i++) {
+        bitVector.push_back(dist(mt) >= 0.5);
+    }
+
+    std::cout << bitVector.str() << std::endl;
+    for (uint32_t i = 0; i < WORD_BIT_SIZE; i++) {
+        std::cout << "rank1(" << i << ") = " << rank1(bitVector.rawData()[0], i) << "  | ";
+        for (uint32_t n = 0; n < i; n++)
+            std::cout << (bitVector.access(n) ? '1' : '0');
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
+
+int bitvector_test(int argc, char *argv[]) {
+
+    test_set_access();
+    test_rank();
+
+    return 0;
+}
+
+ENTRYPOINT(bitvector_test)
