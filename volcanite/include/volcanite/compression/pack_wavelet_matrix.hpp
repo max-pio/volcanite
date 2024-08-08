47e61b1fc23eb5b72b53cf6f90d6df34d443c590
Max Piochowiak <max.piochowiak@kit.edu>
2024-08-08 19:44:51 +0200

    setup packWaveletMatrix

--- volcanite/include/volcanite/compression/pack_wavelet_matrix.hpp
+++ volcanite/include/volcanite/compression/pack_wavelet_matrix.hpp
@@ -15,15 +15,31 @@
 
 #pragma once
 
+#include <cstdint>
+#include <vector>
+
 namespace volcanite {
 
     /// Replaces all 4 bit elements between start4bit (including) and end4bit (excluding) in in_packed with a
     /// wavelet matrix encoded bytestream.
     /// The first 4 bit element start4bit must be the first position in a 32bit memory location.
     /// @return the new end4bit endpoint measured in number of 4 bit elements
-    uint32_t packWaveletMatrix(std::vector<uint32_t> &v, std::size_t start, std::size_t end);
+    uint32_t packWaveletMatrix(std::vector<uint32_t> &v, std::size_t start, std::size_t end) {
+        // Construct a temporary WaveletMatrix Object from the input stream
+        // Then overwrite the vector contents with the wavelet matrix components as follows:
+        // uint32_t Text Size | 4x uint32 ones before level | 4x uint32 zeros in level | FlatRank | Bit Vector
+        // (overwrite header start indices, LOD 0) ?
+        // return length of vector (which can be longer than before!)
+    }
 
-    uint32_t readWMSymbol(uint32_t text_size, uint32_t* bit_vector, uint64_t* FlatRank,
-                          uint32_t ones_before_level[4], uint32_t zeros_in_level[4]);
+    uint32_t accessWMSymbol(uint32_t index, uint32_t text_size, uint64_t* bit_vector, uint64_t* FlatRank,
+                          uint32_t ones_before_level[4], uint32_t zeros_in_level[4]) {
+        // TODO: copy the WaveletMatrix access / [] code here
+    }
+
+    uint32_t rankWSSymbol(uint32_t symbol, int32_t text_size, uint64_t* bit_vector, uint64_t* FlatRank,
+                          uint32_t ones_before_level[4], uint32_t zeros_in_level[4]) {
+        // TODO: copy the WaveletMatrix rank code here
+    }
 
 }
--- volcanite/include/volcanite/compression/wavelet_tree/WaveletMatrix.hpp
+++ volcanite/include/volcanite/compression/wavelet_tree/WaveletMatrix.hpp
@@ -30,6 +30,84 @@ using namespace vvv;
 
 namespace volcanite {
 
+/// \brief Sequential prefix counting algorithm described in
+/// \cite FischerKL2018PWX, based on bottom-up construction.
+///
+/// Copied and adapted from the GPLv3 licensed prefix_counting method from the pasta toolbox Wavelet Tree implementation
+/// https://github.com/pasta-toolbox/wavelet_tree/blob/main/include/pasta/wavelet_tree/prefix_counting.hpp
+///
+/// The construction computes the first level of the wavelet tree/matrix and
+/// the histogram of all characters. Based on the histogram, the borders for
+/// all bit prefixes of all characters on all levels, i.e., the positions of
+/// all bits in the wavelet tree/matrix.
+///
+/// \param op_stream_in pointer to a memory region containing 4 bit operations
+/// \param start4bit the first index in 4 bit elements of the stream
+/// \param end4bit the end index (exclusive) in 4 bit elements of the stream
+/// \param bit_vector_out \c BitVector the wavelet matrix is stored in
+/// (output parameter).
+void prefix_counting(uint32_t* op_stream_in, uint32_t start4bit, uint32_t end4bit, BitVector& bit_vector_out) {
+//    using HistType = std::array<size_t, std::numeric_limits<std::iter_value_t<decltype(begin)>>::max() + 1>;
+//
+//    size_t const text_size = std::distance(begin, end);
+//
+//    auto raw_bv = bit_vector_out.data();
+//
+//    HistType hist = {0};
+//    HistType borders = {0};
+//
+//    uint64_t const mask = 1ULL << (levels - 1);
+//    size_t const shift_first_right = 64 - levels;
+//    auto text_it = begin;
+//    size_t raw_bv_pos = 0;
+//    while (text_it + 64 < end) {
+//        uint64_t bit_block = 0ULL;
+//        for (size_t i = 0; i < 64; ++i, ++text_it) {
+//            bit_block >>= 1;
+//            auto const symbol = *text_it;
+//            ++hist[symbol];
+//            bit_block |= (symbol & mask) << shift_first_right;
+//        }
+//        raw_bv[raw_bv_pos++] = bit_block;
+//    }
+//
+//    uint64_t bit_block = 0ULL;
+//    size_t const remainder = end - text_it;
+//    for (size_t i = 0; i < remainder; ++i) {
+//        auto const symbol = *(text_it + i);
+//        ++hist[symbol];
+//        bit_block >>= 1;
+//        bit_block |= (symbol & mask) << shift_first_right;
+//    }
+//    if (remainder > 0) [[likely]] {
+//        bit_block >>= (64 - remainder);
+//        raw_bv[raw_bv_pos] = bit_block;
+//    }
+//
+//    size_t cur_alphabet_size = (1ULL << levels);
+//    for (size_t level = levels - 1; level > 0; --level) {
+//        cur_alphabet_size >>= 1;
+//        for (size_t i = 0; i < cur_alphabet_size; ++i) {
+//            borders[i] = hist[i << 1] + hist[(i << 1) + 1];
+//        }
+//        std::copy_n(borders.begin(), cur_alphabet_size, hist.begin());
+//
+//
+//        auto const brv = BitReversalPermutation[level];
+//        borders[0] = text_size * level; // brv[0] = 0
+//        for (size_t i = 1; i < cur_alphabet_size; ++i) {
+//            borders[brv[i]] = hist[brv[i - 1]] + borders[brv[i - 1]];
+//        }
+//
+//        size_t const shift_word_for_bit = levels - level - 1;
+//        for (auto it = begin; it < end; ++it) {
+//            auto const symbol_prefix = (*it >> shift_word_for_bit);
+//            size_t const position = borders[symbol_prefix >> 1]++;
+//            raw_bv[position / 64] |= (symbol_prefix & 1ULL) << (position % 64);
+//        }
+//    }
+}
+
 class WaveletMatrix {
 
     private:
@@ -46,11 +124,22 @@ class WaveletMatrix {
             : m_text_size(end4bit - start4bit),
               m_bv(m_text_size * LEVELS, 0u) {
 
+            // construct the concatenated bit vector
             prefix_counting(op_stream_in, start4bit, end4bit, m_bv);
+            // construct flat rank acceleration structure
+            m_fr = new FlatRank(m_bv);
         }
 
         uint32_t access(uint32_t* v, uint32_t start, uint32_t entry_id) const;
         uint32_t rank(uint32_t* v, uint32_t start, uint32_t entry_id) const;
+
+        uint32_t getTextSize() { return m_text_size; }
+        const BitVector* getBitVector() { return &m_bv; }
+        const FlatRank* getFlatRank() { return m_fr; }
+        glm::uvec4 getZerosInLevel() { return glm::uvec4(m_zeros_in_level[0], m_zeros_in_level[1],
+                                                         m_zeros_in_level[2], m_zeros_in_level[3]); }
+        glm::uvec4 getOnesBeforeLevel() { return glm::uvec4(m_ones_before_level[0], m_ones_before_level[1],
+                                                            m_ones_before_level[2], m_ones_before_level[3]); }
 
     };
 
