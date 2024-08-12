ec62545ee34060d6266f22015cf4fa58c1b2a91e
Max Piochowiak <max.piochowiak@kit.edu>
2024-08-12 17:55:49 +0200

    fix invalid detection of empty samples in à-trous iterations

--- volcanite/include/volcanite/renderer/CompressedSegmentationVolumeRenderer.hpp
+++ volcanite/include/volcanite/renderer/CompressedSegmentationVolumeRenderer.hpp
@@ -225,7 +225,7 @@ private:
     glm::vec3 m_bboxMax = glm::vec3(1.f, 1.f, 1.f);
     // denoising
     bool m_bilateral_enabled = false;
-    int m_denoise_filter_kernel_size = 2;
+    int m_denoise_filter_kernel_size = 1;
     float m_difference_depth_denoising = 1.0f;
     float m_spatial_sigma = 2.0f;
     float m_depth_sigma = 0.1f;
