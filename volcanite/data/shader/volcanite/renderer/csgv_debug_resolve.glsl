aeaf6aa430ef1cf0cadf549e75b6c3bd57c88895
Max Piochowiak <max.piochowiak@kit.edu>
2024-10-10 14:42:30 +0200

    use cache visualization only with voxel cache mode

--- volcanite/data/shader/volcanite/renderer/csgv_debug_resolve.glsl
+++ volcanite/data/shader/volcanite/renderer/csgv_debug_resolve.glsl
@@ -12,64 +12,68 @@
 
 // blend a cache visualization over the pixels color
 void DEBUG_img_cache(ivec2 pixel, inout vec4 color, bool enabled) {
-    #ifdef ENALBE_CSGV_DEBUGGING
+#ifdef ENALBE_CSGV_DEBUGGING
     if (!enabled)
-    return;
+        return;
 
-    // map the pixel to a cache cell [region]
     const ivec2 viewport_size = imageSize(inpaintedOutColor);
-    const int size = 4;
-    const uint elems_per_pixel = (CACHE_UINT_SIZE / 2) / (viewport_size.x * viewport_size.y / size);
 
-    const uint idx = elems_per_pixel * uint((pixel.x / size) + (pixel.y / size) * viewport_size.x);
+    #if CACHE_MODE == CACHE_VOXELS
+        // map the pixel to a cache cell [region]
+        const int size = 4;
+        const uint elems_per_pixel = (CACHE_UINT_SIZE / 2) / (viewport_size.x * viewport_size.y / size);
 
-    if (idx >= CACHE_UINT_SIZE / 2)
-    return;
+        const uint idx = elems_per_pixel * uint((pixel.x / size) + (pixel.y / size) * viewport_size.x);
 
-    // accumulate information for all of the pixel's cache cels
-    uint entry_count = 0u;
-    uint visible_count = 0u;
-    vec3 label = vec3(0.f);
-    for (uint i = idx; i < idx + elems_per_pixel; i++) {
-        if (g_cache[i * 2] != INVALID) {
-            entry_count++;
-            if (isLabelVisible(g_cache[i * 2 + 1])) {
-                visible_count++;
-                label += colormap_turbo(float(g_cache[i * 2 + 1] % 96) / 96.f);
-            } else {
-                label += vec3(0.8f, 0.6f, 0.6f);
+        if (idx >= CACHE_UINT_SIZE / 2)
+            return;
+
+        // accumulate information for all of the pixel's cache cels
+        uint entry_count = 0u;
+        uint visible_count = 0u;
+        vec3 label = vec3(0.f);
+        for (uint i = idx; i < idx + elems_per_pixel; i++) {
+            if (g_cache[i * 2] != INVALID) {
+                entry_count++;
+                if (isLabelVisible(g_cache[i * 2 + 1])) {
+                    visible_count++;
+                    label += colormap_turbo(float(g_cache[i * 2 + 1] % 96) / 96.f);
+                } else {
+                    label += vec3(0.8f, 0.6f, 0.6f);
+                }
             }
         }
-    }
 
-    // display the rendering in grayscale in the background
-    color = vec4(vec3(dot(color.rgb, vec3(1.f / 3.f))), color.a);
+        // display the rendering in grayscale in the background
+        color = vec4(vec3(dot(color.rgb, vec3(1.f / 3.f))), color.a);
 
-    // present the cache state as colored output
-    vec3 display;
-    if (entry_count == 0) {
-        display = vec3(1.f);
-    } else {
-        const int mode = 0;
-        switch (mode) {
-            // label
-            case 0:
-            display = label / float(entry_count);
-            break;
-            // occupied entries
-            case 1:
-            display = colormap_viridis(float(entry_count) / float(elems_per_pixel));
-            break;
-            // proportion of invisible labels
-            case 2:
-            display = colormap_viridis(float(visible_count) / float(entry_count));
-            break;
+        // present the cache state as colored output
+        vec3 display;
+        if (entry_count == 0) {
+            display = vec3(1.f);
+        } else {
+            const int mode = 0;
+            switch (mode) {
+                // label
+                case 0:
+                display = label / float(entry_count);
+                break;
+                // occupied entries
+                case 1:
+                display = colormap_viridis(float(entry_count) / float(elems_per_pixel));
+                break;
+                // proportion of invisible labels
+                case 2:
+                display = colormap_viridis(float(visible_count) / float(entry_count));
+                break;
+            }
         }
-    }
 
-    // blend colored cache vis with background
-    const float alpha = 0.8f;
-    color = vec4((1.f - alpha) * color.rgb + alpha * display, 1.f);
+        // blend colored cache vis with background
+        const float alpha = 0.8f;
+        color = vec4((1.f - alpha) * color.rgb + alpha * display, 1.f);
+    #endif
+
 #endif
 }
 
