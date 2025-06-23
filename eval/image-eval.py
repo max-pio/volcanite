8a46fdfede52950802ffdbb80c857324386ec399
Max Piochowiak <max.piochowiak@kit.edu>
2025-06-23 15:35:21 +0200

    add first image-eval evaluation

---
+++ eval/image-eval.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+from pathlib import Path
+from volcanite.volcaniteeval import VolcaniteArg, VolcaniteEvaluation, VolcaniteExec, VolcaniteLogFileCfg, ExistingPolicy
+
+def data_specific_args(data : str) -> list[VolcaniteArg]:
+    vargs = []
+    if data == "Motta2019" or data == "Griesser2022-sample" or data == "H01-wm" or data == "LICONN":
+        vargs.append(VolcaniteArg("--cache-palette"))
+        vargs.append(VolcaniteArg("--cache-size 2048"))
+    else:
+        vargs.append(VolcaniteArg("--cache-size 4095"))
+    return vargs
+
+if __name__ == "__main__":
+
+    # set up the evaluation output directory and the log files
+    evaluation_name = Path(__file__).stem
+    evaluation = VolcaniteEvaluation(eval_out_directory=f"./results/{evaluation_name}/", existing_policy=ExistingPolicy.DELETE,
+                                        eval_name=evaluation_name,
+                                        log_files=[VolcaniteLogFileCfg(f"{evaluation_name}.csv",
+                                                              fmts=["{name},{frame_min_ms},{frame_avg_ms},{frame_max_ms},{frame_sdv_ms},"
+                                                                     + ",".join("{:02}".format(i) for i in range(16))],
+                                                              headers=["Shading Mode,frame min [ms],frame avg [ms],frame max [ms],stdv,"
+                                                                        + ",".join("Frame {:02}".format(i) for i in range(16))])],
+                                        enable_log=True, dry_run=False)
+
+    volcanite = VolcaniteExec(evaluation, build_subdir="cmake-build-release")
+    volcanite.checkout_and_build()
+
+    # print evaluation information to console
+    print("\n" + volcanite.info_str())
+    print("\n".join(volcanite.logs_info_str()))
+
+    print("Data sets: " + ', '.join(sorted([args.identifier for args in VolcaniteArg.args_csgv_datasets.values()])))
+
+    # log a time stamp
+    evaluation.get_log().log_manual("# " + datetime.now().strftime("%Y.%m.%d-%H:%M:%S"))
+
+    # iterate over all configuration combinations and execute Volcanite
+    for arg_data in VolcaniteArg.args_csgv_datasets.values():
+
+        # log data set name
+        evaluation.get_log().log_manual("# " + arg_data.identifier)
+
+        # load the .vcfg file for the data set (default perspective)
+        render_config = VolcaniteArg.arg_config_import([arg_data])
+
+        for arg_shading in VolcaniteArg.args_shading.values():
+
+            # evaluate timings and export an image
+            image_export = VolcaniteArg.arg_image_export([arg_data, arg_shading])
+
+            vargs = [arg_data, render_config, arg_shading, image_export] + data_specific_args(arg_data.identifier)
+
+            # log a summary line of all arguments
+            evaluation.get_log().log_manual("# " + VolcaniteArg.concat_ids(vargs))
+
+            # execute Volcanite and pass the Volcanite log file into which the results are appended
+            volcanite.exec(vargs, eval_name=arg_shading.identifier)
+
+        break
+
+
+    # create a copy of the log file with correct formatting
+    # evaluation.get_log().create_formatted_copy(evaluation.eval_out_directory / Path("image-eval.csv"), newline_separator="\\\\")
+
--- eval/render-eval.py
+++ eval/render-eval.py
@@ -1,36 +1,47 @@
 from pathlib import Path
-import volcanite.volcaniteeval as ve
+from volcanite.volcaniteeval import VolcaniteArg, VolcaniteEvaluation, VolcaniteExec, VolcaniteLogFileCfg, ExistingPolicy
+
+def data_specific_args(data : str) -> list[VolcaniteArg]:
+    vargs = []
+    if data == "Motta2019" or data == "Griesser2022-sample" or data == "H01-wm" or data == "LICONN":
+        vargs.append(VolcaniteArg("--cache-palette"))
+        vargs.append(VolcaniteArg("--cache-size 2048"))
+    else:
+        vargs.append(VolcaniteArg("--cache-size 4095"))
+    return vargs
 
 if __name__ == "__main__":
+
     # set up the evaluation output directory and the log files
-    evaluation = ve.VolcaniteEvaluation(eval_out_directory="./my_test_eval", existing_policy=ve.ExistingPolicy.DELETE,
-                                        eval_name="my_test_eval",
-                                        log_files=[ve.VolcaniteLogFileCfg("results.csv",
-                                                              fmts=["{name},{comprate_pcnt:.3},{frame_min_ms},{frame_avg_ms}"],
-                                                              headers=["Name,Compression Rate [%],frame avg [ms]"])],
+    evaluation = VolcaniteEvaluation(eval_out_directory="./results/render-eval/", existing_policy=ExistingPolicy.DELETE,
+                                        eval_name="render-eval",
+                                        log_files=[VolcaniteLogFileCfg("render-eval.csv",
+                                                              fmts=["{name},{frame_min_ms},{frame_avg_ms},{frame_max_ms}"],
+                                                              headers=["Name,frame min [ms],frame avg [ms],frame max [ms]"])],
                                         enable_log=True, dry_run=False)
 
-    volcanite = ve.VolcaniteExec(evaluation, build_subdir="cmake-build-release")
+    volcanite = VolcaniteExec(evaluation, build_subdir="cmake-build-release")
     volcanite.checkout_and_build()
 
     # print evaluation information to console
-    print(volcanite.info_str())
+    print("\n" + volcanite.info_str())
     print("\n".join(volcanite.logs_info_str()))
 
-    print("Data sets: " + ', '.join([args.identifier for args in ve.VolcaniteArg.arg_dataset]))
-    exit(0)
+    print("Data sets: " + ', '.join(sorted([args.identifier for args in VolcaniteArg.args_csgv_datasets.values()])))
+
+    # constant arguments
+    video_cfg = VolcaniteArg.arg_video_cfg(rotation=(-360, 0), zoom=(2, 0), duration=600, interpolant="smooth", edge=(0.2, 0.8),
+                                              duration_is_seconds=False, output_framerate=0)
 
     # iterate over all configuration combinations and execute Volcanite
-    for arg_data in ve.VolcaniteArg.args_datasynth.values():
+    for arg_data in VolcaniteArg.args_csgv_datasets.values():
 
-        # combine several VolcaniteArgs
-        vargs = [arg_data, ve.VolcaniteArg.args_encoding["rANS"]]
-
-        # add an image export arg that uses the IDs of the previous args as file names
-        vargs.append(ve.VolcaniteArg.arg_image_export(vargs))
+        video_export = VolcaniteArg.arg_video_export([arg_data])
+        render_config = VolcaniteArg.arg_config_import([arg_data])
+        vargs = [arg_data, render_config, video_cfg, video_export] + data_specific_args(arg_data.identifier)
 
         # manually log a comment line to the log file
-        evaluation.get_log().log_manual("#Running evaluation: " + ve.VolcaniteArg.concat_ids(vargs))
+        evaluation.get_log().log_manual("# " + VolcaniteArg.concat_ids(vargs))
 
         # execute Volcanite and pass the Volcanite log file into which the results are appended
         volcanite.exec(vargs)
