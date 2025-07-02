64e8ab9dab8a2be724fd51a8bf0eae06f277dbb6
Max Piochowiak <max.piochowiak@kit.edu>
2025-07-02 13:56:43 +0200

    eval: validreq-eval includes more data, video

--- eval/validreq-eval.py
+++ eval/validreq-eval.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                                         eval_name=evaluation_name,
                                         log_files=[VolcaniteLogFileCfg(f"{evaluation_name}.csv",
                                                               fmts=["{frame_min_ms},{frame_avg_ms},{frame_max_ms},{frame_sdv_ms},{frame_med_ms}"],
-                                                              headers=["Data Set,Valid Requests Only,frame min [ms],frame avg [ms],frame max [ms],stdv,frame med [ms]"])],
+                                                              headers=["Data Set,Shader Defi,frame min [ms],frame avg [ms],frame max [ms],stdv,frame med [ms]"])],
                                         enable_log=True, dry_run=False)
 
     volcanite = VolcaniteExec(evaluation, build_subdir="cmake-build-release")
@@ -27,13 +27,15 @@ if __name__ == "__main__":
     # log a time stamp
     evaluation.get_log().log_manual("# " + datetime.now().strftime("%Y.%m.%d-%H:%M:%S"))
 
+    arg_image_eval = VolcaniteArg.arg_image_eval_cfg(1024)
     arg_video_cfg = VolcaniteArg.arg_video_eval_cfg(rotation=(-360, 0), zoom=(2, -0.2), duration=600, interpolant="smooth", edge=(0.2, 0.8),
                                                     duration_is_seconds=False, output_framerate=0)
 
     # iterate over all configuration combinations and execute Volcanite
     for arg_data in VolcaniteArg.args_csgv_datasets.values():
 
-        if arg_data.identifier != "pa66" and arg_data.identifier.lower() != "motta2019-small" and arg_data.identifier.lower() != "h01-wm":
+        if arg_data.identifier != "pa66" and arg_data.identifier.lower() != "motta2019-small" and arg_data.identifier.lower() != "h01-wm"\
+         and arg_data.identifier.lower() != "griesser2022-sample" and arg_data.identifier.lower() != "cells":
             continue
 
         # log data set name
@@ -42,7 +44,7 @@ if __name__ == "__main__":
         # load the .vcfg file for the data set (default perspective)
         arg_vcfg = VolcaniteArg.arg_config_import([arg_data])
 
-        for arg_shaderdef in [VolcaniteArg([], "", 80),
+        for arg_shaderdef in [VolcaniteArg([], "none", 80),
                               VolcaniteArg(["--shader-def", "VALID_RAY_REQUEST_ONLY"], "valr", 80),
                               VolcaniteArg(["--shader-def", "COMPUTE_LOD_AT_RENDER_TIME"], "rlod", 80),
                               VolcaniteArg(["--shader-def", "VALID_RAY_REQUEST_ONLY,COMPUTE_LOD_AT_RENDER_TIME"], "valr-rlod", 80)]:
@@ -52,9 +54,7 @@ if __name__ == "__main__":
             # evaluate timings and export an image
             arg_video_export = VolcaniteArg.arg_video_export([arg_data, arg_shaderdef])
 
-            arg_image_eval = VolcaniteArg.arg_image_eval_cfg(1024)
-
-            vargs = [arg_data, arg_vcfg, arg_image_eval, arg_shading, arg_shaderdef] \
+            vargs = [arg_data, arg_vcfg, arg_video_cfg, arg_video_export, arg_shading, arg_shaderdef] \
                         + data_specific_args(arg_data.identifier)
             
             # log a summary line of all arguments
