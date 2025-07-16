53cb05ee66184a16b95f1f3c0f97182a8951fc60
Max Piochowiak <max.piochowiak@kit.edu>
2025-07-16 18:44:43 +0200

    fix: missing computeVolumeInfo when verifying imported .csgv

--- volcanite/src/volcanite/compression/CompressedSegmentationVolume.cpp
+++ volcanite/src/volcanite/compression/CompressedSegmentationVolume.cpp
@@ -861,6 +861,7 @@ bool CompressedSegmentationVolume::importFromFile(const std::string &path, bool 
             return false;
         } else {
             Logger(Debug) << "verifying: ok (" << verifyTimer.elapsed() << "s)";
+            computeVolumeInfo();
             return true;
         }
     }
