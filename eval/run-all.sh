be6da23e8db3a838a8b3b09a4eae53ed1617767c
Max Piochowiak <max.piochowiak@kit.edu>
2026-01-27 13:33:34 +0100

    add: eval/run-all.sh route output to logfile

--- eval/run-all.sh
+++ eval/run-all.sh
@@ -16,28 +16,46 @@ fi
 
 source ../.venv/bin/activate
 
+# create timestamped logfile name
+timestamp="$(date +'%Y-%m-%d_%H-%M-%S')"
+logfile="eval-log-${timestamp}.log"
+# redirect all outputs to the logfile (and keep on console)
+exec > >(tee -a "$logfile") 2>&1
+
+echo "<<<<<<< image-eval.py >>>>>>>"
 python3 ./image-eval.py
 sleep 30
+echo "<<<<<<< video-eval.py >>>>>>>"
 python3 ./video-eval.py
 sleep 30
+echo "<<<<<<< vram-eval.py >>>>>>>"
 python3 ./vram-eval.py
 sleep 30
+echo "<<<<<<< resolve-eval.py >>>>>>>"
 python3 ./resolve-video-eval.py
 sleep 30
 
+echo "<<<<<<< cache-palette-eval.py >>>>>>>"
 python3 ./cache-palette-eval.py
 sleep 30
+echo "<<<<<<< cache-pathlimit-eval.py >>>>>>>"
 python3 ./cache-pathlimit-eval.py
 sleep 30
+echo "<<<<<<< cache-reqlimit-eval.py >>>>>>>"
 python3 ./cache-reqlimit-eval.py
 sleep 30
+echo "<<<<<<< cache-rngcontrol-eval.py >>>>>>>"
 python3 ./cache-rngcontrol-eval.py
 sleep 30
 
+echo "<<<<<<< csgv-eval.py >>>>>>>"
 python3 ./csgv-eval.py
 sleep 30
+echo "<<<<<<< csgv-ablation-eval.py >>>>>>>"
 python3 ./csgv-ablation-eval.py
 sleep 30
+echo "<<<<<<< deltaoperation-b64-eval.py >>>>>>>"
 python3 ./deltaoperation-b64-eval.py
 
-echo "all finished."
+echo "\n-------------------"
+echo "all done."
