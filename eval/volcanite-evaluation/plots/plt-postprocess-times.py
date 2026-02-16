9aa3bce4126bc68a6e4a10491db3a3e7e0af2871
Max Piochowiak <max.piochowiak@kit.edu>
2026-02-16 14:34:03 +0100

    add: filesize-eval

--- eval/volcanite-evaluation/plots/plt-postprocess-times.py
+++ eval/volcanite-evaluation/plots/plt-postprocess-times.py
@@ -12,6 +12,7 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import numpy as np
 
 from timingplots import plot_gpu_timings
