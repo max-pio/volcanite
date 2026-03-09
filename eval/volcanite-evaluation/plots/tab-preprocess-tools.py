b2153da779f31f692521c84a0e7e14ec015ff337
Max Piochowiak <max.piochowiak@kit.edu>
2026-03-09 11:26:34 +0100

    eval: tab-preprocess prints human readable

--- eval/volcanite-evaluation/plots/tab-preprocess-tools.py
+++ eval/volcanite-evaluation/plots/tab-preprocess-tools.py
@@ -25,6 +25,41 @@ from matplotlib.patches import Patch
 
 print("--------------\nTabulating Neuroglancer\\VTK\\Volcanite Preprocessing")
 
+HUMAN_READABLE = True
+
+def format_duration(total_seconds: float) -> str:
+    # Separate integer and fractional part
+    int_seconds = int(total_seconds)
+    frac = total_seconds - int_seconds  # keep as fraction of a second
+
+    days, rem = divmod(int_seconds, 24 * 60 * 60)
+    hours, rem = divmod(rem, 60 * 60)
+    minutes, seconds = divmod(rem, 60)
+
+    # Add fractional part back to the seconds
+    seconds_with_frac = seconds + frac
+
+    parts = []
+    if days:
+        parts.append(f"{days}d")
+    if hours:
+        parts.append(f"{hours}h")
+    if minutes:
+        parts.append(f"{minutes}m")
+
+    sec_str = f"{seconds_with_frac:04.1f}" # .rstrip("0").rstrip(".")
+    if sec_str or not parts:
+        parts.append(f"{sec_str}s")
+
+    return " ".join(parts)
+
+def add_human_read_timings(df: pd.DataFrame) -> pd.DataFrame:
+    s_columns = [col for col in df.columns if col.endswith('[s]')]
+    for col in s_columns:
+        new_col = f"{col} [h]"
+        df[new_col] = df[col].apply(format_duration)
+    return df
+
 csgv_df = pd.read_csv("../results/compression-eval/compression-eval.csv", comment="#")
 csgv_evaluated_data = csgv_df["Data Set"].unique()
 csgv_data_set_count = len(csgv_evaluated_data)
@@ -34,6 +69,7 @@ csgv_df["Import IO Time [s]"] = csgv_df["Compression Time Total with IO [s]"] - 
 csgv_df.sort_values(by="Orig Size [GB]", inplace=True, ignore_index=True)
 csgv_df["table_name"] = ""; csgv_df.loc[0, "table_name"] = f"\\multirow{{{csgv_data_set_count}}}{{*}}{{\\rotatebox[origin=c]{{90}}{{Volcanite}}}}"
 csgv_df["empty"] = ""
+csgv_df = add_human_read_timings(csgv_df)
 
 vtk_df = pd.read_csv("../results/vtk-eval/vtk-eval.csv", comment="#")
 vtk_evaluated_data = vtk_df["Data Set"].unique()
@@ -42,6 +78,7 @@ vtk_df["empty"] = ""
 vtk_df = vtk_df.merge(csgv_df[["Data Set", "Orig Size [GB]"]], on="Data Set", how="left")
 vtk_df.sort_values(by="Orig Size [GB]", inplace=True, ignore_index=True)
 vtk_df["table_name"] = ""; vtk_df.loc[0, "table_name"] = f"\\multirow{{{vtk_data_set_count}}}{{*}}{{\\rotatebox[origin=c]{{90}}{{VTK}}}}"
+vtk_df = add_human_read_timings(vtk_df)
 
 ng_df = pd.read_csv("../results/neuroglancer-eval/neuroglancer-eval.csv", comment="#")
 ng_evaluated_data = ng_df["Data Set"].unique()
@@ -53,6 +90,7 @@ ng_df["empty"] = ""
 ng_df = ng_df.merge(csgv_df[["Data Set", "Orig Size [GB]"]], on="Data Set", how="left")
 ng_df.sort_values(by="Orig Size [GB]", inplace=True, ignore_index=True)
 ng_df["table_name"] = ""; ng_df.loc[0, "table_name"] = f"\\multirow{{{ng_data_set_count}}}{{*}}{{\\rotatebox[origin=c]{{90}}{{Neuroglancer}}}}"
+ng_df = add_human_read_timings(ng_df)
 
 file_df = pd.read_csv("../results/filesize-eval/filesize-eval.csv", comment="#")
 vtk_df = vtk_df.merge(file_df[["Data Set", "hdf5 (gzip) Filesize [GB]"]], on="Data Set", how="left")
@@ -64,48 +102,93 @@ times_path.parent.mkdir(parents=True, exist_ok=True)
 with open(times_path, 'w') as f:
     f.write("\\begin{tabular}{cl|rrrr|rr}\n")
 
-    # Volcanite
-    f.write("% For Volcanite, compression only (without IO) includes freq. prepass and main pass.\n")
-    f.write(r"& & \multicolumn{4}{l|}{Preprocessing Times [s]} & \multicolumn{2}{l}{File Sizes [GB]} \\" + "\n")
-    f.write("& Data Set & Compr. only& File IO& Total with IO& TTFF& Direct & gzip \\\\\n")
-    f.write("\\midrule\n")
-    f.write(df_to_latex_rows(csgv_df[["table_name", "Data Set", "Compression Time Total [s]", "Import IO Time [s]",
-                                      "Compression Time Total with IO [s]", "Time To First Frame [s]",
-                                      "CSGV Filesize [GB]", "CSGV (gzip) Filesize [GB]"]],
-                             ["{}", "\\dataNameFromCSV{{{}}}", "{:.3f}", "{:.3f}", "{:.3f}", "{:.3f}", "{:.3f}", "{:.3f}"]))
+    if HUMAN_READABLE:
+        # Volcanite
+        f.write("% For Volcanite, compression only (without IO) includes freq. prepass and main pass.\n")
+        f.write(r"& & \multicolumn{4}{l|}{Preprocessing Times [s]} & \multicolumn{2}{l}{File Sizes [GB]} \\" + "\n")
+        f.write("& Data Set & Compr. only& File IO& Total with IO& TTFF& Direct & gzip \\\\\n")
+        f.write("\\midrule\n")
+        f.write(df_to_latex_rows(
+            csgv_df[["table_name", "Data Set", "Compression Time Total [s] [h]", "Import IO Time [s] [h]",
+                     "Compression Time Total with IO [s] [h]", "Time To First Frame [s] [h]",
+                     "CSGV Filesize [GB]", "CSGV (gzip) Filesize [GB]"]],
+            ["{}", "\\dataNameFromCSV{{{}}}", "{}", "{}", "{}", "{}", "{:.3f}", "{:.3f}"]))
 
-    # VTK
-    f.write(r"\multicolumn{8}{c}{}\\" + "\n")
-    f.write("% For VTK, preprocessing time is the IO file import. TTFF includes GPU uploads etc.\n")
-    f.write(r"& & \multicolumn{4}{l|}{Preprocessing Times [s]} & \multicolumn{2}{l}{File Sizes [GB]} \\" + "\n")
-    f.write("& Data Set & & & Total with IO & TTFF & Direct & gzip \\\\\n")
-    f.write("\\midrule\n")
-    f.write(df_to_latex_rows(vtk_df[["table_name", "Data Set", "empty", "empty",
-                                     "preprocess IO time [s]","time to first frame [s]",
-                                     "Orig Size [GB]", "hdf5 (gzip) Filesize [GB]"]],
-                             ["{}", "\\dataNameFromCSV{{{}}}", "{}", "{}", "{:.3f}", "{:.3f}", "{:.3f}", "{:.3f}"]))
-    missing_vtk = [x for x in csgv_evaluated_data if x not in vtk_evaluated_data]
-    if len(missing_vtk) > 0:
-        f.write(r"& \dots & \multicolumn{4}{r|}{\textcolor{gray}{\tiny "
-                + ", ".join([f"\\dataNameFromCSV{{{d}}}" for d in missing_vtk])
-                + r": out of memory}} & & \\")
+        # VTK
+        f.write(r"\multicolumn{8}{c}{}\\" + "\n")
+        f.write("% For VTK, preprocessing time is the IO file import. TTFF includes GPU uploads etc.\n")
+        f.write(r"& & \multicolumn{4}{l|}{Preprocessing Times [s]} & \multicolumn{2}{l}{File Sizes [GB]} \\" + "\n")
+        f.write("& Data Set & & & Total with IO & TTFF & Direct & gzip \\\\\n")
+        f.write("\\midrule\n")
+        f.write(df_to_latex_rows(vtk_df[["table_name", "Data Set", "empty", "empty",
+                                         "preprocess IO time [s] [h]", "time to first frame [s] [h]",
+                                         "Orig Size [GB]", "hdf5 (gzip) Filesize [GB]"]],
+                                 ["{}", "\\dataNameFromCSV{{{}}}", "{}", "{}", "{}", "{}", "{:.3f}", "{:.3f}"]))
+        missing_vtk = [x for x in csgv_evaluated_data if x not in vtk_evaluated_data]
+        if len(missing_vtk) > 0:
+            f.write(r"& \dots & \multicolumn{4}{r|}{{\footnotesize "
+                    + ", ".join([f"\\dataNameFromCSV{{{d}}}" for d in missing_vtk])
+                    + r": out of memory}} & & \\")
 
-    # Neuroglancer
-    f.write(r"\multicolumn{8}{c}{}\\" + "\n")
-    f.write("% For neuroglancer, all timings are with IO included (not separable).\n")
-    f.write(r"& & \multicolumn{4}{l|}{Preprocessing Times [s]} & \multicolumn{2}{l}{File Sizes [GB]} \\" + "\n")
-    f.write("& Data Set & Compr. Segm. & Meshing & Total with IO & & Direct & gzip \\\\\n")
-    f.write("\\midrule\n")
-    f.write(df_to_latex_rows(ng_df[["table_name", "Data Set", "Precomputed Time [s]", "Meshing Time [s]",
-                                    "preprocess IO time [s]", "empty",
-                                    "Total Size [GB]", "Total Size (gzip) [GB]"]],
-                             ["{}", "\\dataNameFromCSV{{{}}}", "{:.3f}", "{:.3f}", "{:.3f}", "{}", "{:.3f}", "{:.3f}"]))
+        # Neuroglancer
+        f.write(r"\multicolumn{8}{c}{}\\" + "\n")
+        f.write("% For neuroglancer, all timings are with IO included (not separable).\n")
+        f.write(r"& & \multicolumn{4}{l|}{Preprocessing Times [s]} & \multicolumn{2}{l}{File Sizes [GB]} \\" + "\n")
+        f.write("& Data Set & Compr. Segm. & Meshing & Total with IO & & Direct & gzip \\\\\n")
+        f.write("\\midrule\n")
+        f.write(df_to_latex_rows(ng_df[["table_name", "Data Set", "Precomputed Time [s] [h]", "Meshing Time [s] [h]",
+                                        "preprocess IO time [s] [h]", "empty",
+                                        "Total Size [GB]", "Total Size (gzip) [GB]"]],
+                                 ["{}", "\\dataNameFromCSV{{{}}}", "{}", "{}", "{}", "{}", "{:.3f}", "{:.3f}"]))
 
-    missing_ng = [x for x in csgv_evaluated_data if x not in ng_evaluated_data]
-    if len(missing_ng) > 0:
-        f.write(r"& \dots & \multicolumn{4}{r|}{\textcolor{gray}{\tiny "
-                + ", ".join([f"\\dataNameFromCSV{{{d}}}" for d in missing_ng])
-                + r": out of memory}} & & \\")
+        missing_ng = [x for x in csgv_evaluated_data if x not in ng_evaluated_data]
+        if len(missing_ng) > 0:
+            f.write(r"& \dots & \multicolumn{4}{r|}{{\footnotesize "
+                    + ", ".join([f"\\dataNameFromCSV{{{d}}}" for d in missing_ng])
+                    + r": out of memory}} & & \\")
+    else:
+        # Volcanite
+        f.write("% For Volcanite, compression only (without IO) includes freq. prepass and main pass.\n")
+        f.write(r"& & \multicolumn{4}{l|}{Preprocessing Times [s]} & \multicolumn{2}{l}{File Sizes [GB]} \\" + "\n")
+        f.write("& Data Set & Compr. only& File IO& Total with IO& TTFF& Direct & gzip \\\\\n")
+        f.write("\\midrule\n")
+        f.write(df_to_latex_rows(csgv_df[["table_name", "Data Set", "Compression Time Total [s]", "Import IO Time [s]",
+                                          "Compression Time Total with IO [s]", "Time To First Frame [s]",
+                                          "CSGV Filesize [GB]", "CSGV (gzip) Filesize [GB]"]],
+                                 ["{}", "\\dataNameFromCSV{{{}}}", "{:.3f}", "{:.3f}", "{:.3f}", "{:.3f}", "{:.3f}", "{:.3f}"]))
+
+        # VTK
+        f.write(r"\multicolumn{8}{c}{}\\" + "\n")
+        f.write("% For VTK, preprocessing time is the IO file import. TTFF includes GPU uploads etc.\n")
+        f.write(r"& & \multicolumn{4}{l|}{Preprocessing Times [s]} & \multicolumn{2}{l}{File Sizes [GB]} \\" + "\n")
+        f.write("& Data Set & & & Total with IO & TTFF & Direct & gzip \\\\\n")
+        f.write("\\midrule\n")
+        f.write(df_to_latex_rows(vtk_df[["table_name", "Data Set", "empty", "empty",
+                                         "preprocess IO time [s]","time to first frame [s]",
+                                         "Orig Size [GB]", "hdf5 (gzip) Filesize [GB]"]],
+                                 ["{}", "\\dataNameFromCSV{{{}}}", "{}", "{}", "{:.3f}", "{:.3f}", "{:.3f}", "{:.3f}"]))
+        missing_vtk = [x for x in csgv_evaluated_data if x not in vtk_evaluated_data]
+        if len(missing_vtk) > 0:
+            f.write(r"& \dots & \multicolumn{4}{r|}{{\footnotesize "
+                    + ", ".join([f"\\dataNameFromCSV{{{d}}}" for d in missing_vtk])
+                    + r": out of memory}} & & \\")
+
+        # Neuroglancer
+        f.write(r"\multicolumn{8}{c}{}\\" + "\n")
+        f.write("% For neuroglancer, all timings are with IO included (not separable).\n")
+        f.write(r"& & \multicolumn{4}{l|}{Preprocessing Times [s]} & \multicolumn{2}{l}{File Sizes [GB]} \\" + "\n")
+        f.write("& Data Set & Compr. Segm. & Meshing & Total with IO & & Direct & gzip \\\\\n")
+        f.write("\\midrule\n")
+        f.write(df_to_latex_rows(ng_df[["table_name", "Data Set", "Precomputed Time [s]", "Meshing Time [s]",
+                                        "preprocess IO time [s]", "empty",
+                                        "Total Size [GB]", "Total Size (gzip) [GB]"]],
+                                 ["{}", "\\dataNameFromCSV{{{}}}", "{:.3f}", "{:.3f}", "{:.3f}", "{}", "{:.3f}", "{:.3f}"]))
+
+        missing_ng = [x for x in csgv_evaluated_data if x not in ng_evaluated_data]
+        if len(missing_ng) > 0:
+            f.write(r"& \dots & \multicolumn{4}{r|}{{\footnotesize "
+                    + ", ".join([f"\\dataNameFromCSV{{{d}}}" for d in missing_ng])
+                    + r": out of memory}} & & \\")
 
     f.write("\\end{tabular}\n")
 
